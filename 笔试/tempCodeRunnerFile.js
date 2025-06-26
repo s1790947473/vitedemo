16260e0fd60754555860fc28e77b1f0196cc0d46
s1790947473 <2542343510@qq.com>
2025-06-26 20:59:19 +0800

    在draft中学习 js异步， async await promis

--- 笔试/tempCodeRunnerFile.js
+++ 笔试/tempCodeRunnerFile.js
@@ -19,3 +19,13 @@ async function async3() {
 console.log("start");
 async1();
 console.log("end");
+
+// 1. start
+// 2. async1 start
+// 3. await async2执行，async1暂停挂起， async2
+// 4. await async2让出主线程,将后续async2 end注册微任务,回到主线程
+// 5. 主线程end
+// 6. 同步代码完成,进入微任务,async2 end
+// 7. async2完毕，async1继续执行，async1 end
+// 8. 同理await async3执行，async1暂停挂起，async3
+// 9. async3 final
